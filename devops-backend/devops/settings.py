8e786b51981cc4fcf392a0f63a443a3f8d6dc13b
anusuya-c <anusuyachudal3@gmail.com>
2025-04-08 23:10:15 +0545

    Connect to postgres successfully.

--- devops-backend/devops/settings.py
+++ devops-backend/devops/settings.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = 'devops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'datbase',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    
     }
 }
 
