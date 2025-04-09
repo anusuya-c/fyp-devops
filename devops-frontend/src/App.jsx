1454679633d839a64b2f375628bc24e29f40f037
anusuya-c <anusuyachudal3@gmail.com>
2025-04-09 23:03:59 +0545

    Update App.jsx

--- devops-frontend/src/App.jsx
+++ devops-frontend/src/App.jsx
@@ -1,17 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HomePage from './pages/Home'
-import { AuthProvider } from './auth/AuthContext'
+import "./App.css";
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+
+import { MantineProvider } from "@mantine/core";
+
+import { AuthProvider, useAuth } from "./auth/AuthContext";
+import { Notifications } from "@mantine/notifications";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import HomePage from "./pages/Home";
+import RegisterPage from "./pages/auth/Register";
+import LoginPage from "./pages/auth/Login";
+import { Navigate } from "react-router";
 
 function App() {
+  const ProtectedRoute = ({ Component }) => {
+    const { isAuthenticated } = useAuth();
+    return isAuthenticated ? Component : <Navigate to="/login" replace />;
+  };
+
+  const LoginRedirect = ({ children }) => {
+    const { isAuthenticated } = useAuth();
+    return isAuthenticated ? <Navigate to="/home" replace /> : children;
+  };
 
   return (
-    <AuthProvider>
-      <HomePage />
-    </AuthProvider>
-  )
+    <MantineProvider>
+      <Notifications />
+      <AuthProvider>
+        <Notifications />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/register"
+              element={
+                <LoginRedirect>
+                  <RegisterPage />
+                </LoginRedirect>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <LoginRedirect>
+                  <LoginPage />
+                </LoginRedirect>
+              }
+            />
+
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/home"
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </MantineProvider>
+  );
 }
 
-export default App
+export default App;
